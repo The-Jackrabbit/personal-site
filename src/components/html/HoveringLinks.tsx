3cdd146c9eb9ffb37d36004a4b41a3e152c417b5
The-Jackrabbit <lsm5fm@virginia.edu>
2022-10-14 20:38:03 -0700

    cleanup credits

--- src/components/html/HoveringLinks.tsx
+++ src/components/html/HoveringLinks.tsx
@@ -8,17 +8,17 @@ export const HoveringLinks: React.FC = () =>{
   return  (
     <Html center position={[-0.5,2.8,0]}>
       <div className="text-right text-5xl text-white w-72 bg-transparent p-8">
+        <a className="mb-16 normal-case block" href="https://www.linkedin.com/in/luke-masters-525944120/">linkedin</a>
+				<a className="mb-16 normal-case block" href="https://github.com/The-Jackrabbit">github</a>
+				<a className="normal-case block" href="mailto:lukemastersva@gmail.com">contact</a>
+        <button className="text-xs " onClick={() => setIsOpen(!isOpen)}>credits</button>
         {isOpen && (
-          <div onClick={() => setIsOpen(!isOpen)} className="text-xs w-screen text-green-700 bg-transparent p-8">
+          <div onClick={() => setIsOpen(!isOpen)} className="text-xs bg-transparent p-8">
             <p>This work is based on {'"'}Folding Chair - Low Poly{'"'} (https://sketchfab.com/3d-models/folding-chair-low-poly-54bd0acd7c524d678128367a25a0f504) by Jeremy E. Grayson (https://sketchfab.com/JeremyGrayson) licensed under CC-BY-4.0 (http://creativecommons.org/licenses/by/4.0/)</p>
 
             <p>This work is based on {'"'}Grass{'"'} (https://sketchfab.com/3d-models/grass-4b800e07ea3543e3870ad5e53b39d825) by MauroGonzalezA (https://sketchfab.com/MauroGonzalezA) licensed under CC-BY-4.0 (http://creativecommons.org/licenses/by/4.0/)</p>
           </div>
         )}
-        <a className="mb-16 normal-case block" href="https://www.linkedin.com/in/luke-masters-525944120/">linkedin</a>
-				<a className="mb-16 normal-case block" href="https://github.com/The-Jackrabbit">github</a>
-				<a className="normal-case block" href="mailto:lukemastersva@gmail.com">contact</a>
-        {!isOpen && <button className="text-xs mb-32 " onClick={() => setIsOpen(!isOpen)}>credits</button>}
       </div>
     </Html>
   );
